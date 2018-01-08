cbf4509c35370122f1595d2323e8510ffea41c8c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 12:40:03 -0800

    removed special characters and added sys module

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,7 @@
 # put your code here.
+import sys
+
+
 def word_count_in_dict(file_name):
     word_count = {}
     my_file = open(file_name)
@@ -6,10 +9,14 @@ def word_count_in_dict(file_name):
         line = line.rstrip()
         words = line.split(" ")
         for word in words:
+            word = word.lower()
+            word = word.strip("?")
+            word = word.strip(".")
+            word = word.strip(",")
             word_count[word] = word_count.get(word, 0) + 1
     for word, count in word_count.iteritems():
         print word, count
     my_file.close()
 
 
-word_count_in_dict("twain.txt")
+word_count_in_dict(sys.argv[1])
