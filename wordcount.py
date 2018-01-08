140433dbf214537fca2521f79c95ed626a4682bd
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 13:03:22 -0800

    collections.counter

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,6 @@
 # put your code here.
 import sys
+from collections import Counter
 
 
 def word_count_in_dict(file_name):
@@ -19,4 +20,17 @@ def word_count_in_dict(file_name):
     my_file.close()
 
 
-word_count_in_dict(sys.argv[1])
+#word_count_in_dict(sys.argv[1])
+
+
+def counter_based_dict(file_name):
+
+    my_file = open(file_name)
+    huge_line = my_file.read()
+    #print huge_line
+    c = Counter(huge_line.split())
+
+    print c
+
+
+counter_based_dict("test.txt")
